19a4c6e79f6bb0c2598c587bb3a7d4f5829ebdd9
Hadi Bakhshi <hadibakhshi277@gmail.com>
2024-11-01 22:18:28 +0330

    Add project files.

---
+++ PortfolioBackend/Entities/AboutMe.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace PortfolioBackend.Entities;
+
+public class AboutMe
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+
+    [BsonElement("description")]
+    public required string Description { get; set; }
+}
